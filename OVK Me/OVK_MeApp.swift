b913812f1896d9d18509d20389911ef6dadfd136
El psy congro desu <94683384+el-psy-congro-desu@users.noreply.github.com>
2022-07-20 01:59:20 +0500

    Initial Commit

---
+++ OVK Me/OVK_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OVK_MeApp.swift
+//  OVK Me
+//
+//  Created by Vladimir Lapskiy on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct OVK_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
